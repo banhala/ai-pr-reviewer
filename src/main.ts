9f01a6b920314c9529d8dcd398b3a06e00789c53
장영환(Yeonghwan Chang) <yeonghwan.chang@a-bly.com>
2025-03-21 15:49:56 +0900

    refactor: issue comment의 경우에 full review

--- src/main.ts
+++ src/main.ts
@@ -71,8 +71,9 @@ async function run(): Promise<void> {
   try {
     // check if the event is pull_request
     if (
-      process.env.GITHUB_EVENT_NAME === 'pull_request' ||
-      process.env.GITHUB_EVENT_NAME === 'pull_request_target'
+      // process.env.GITHUB_EVENT_NAME === 'pull_request' ||
+      // process.env.GITHUB_EVENT_NAME === 'pull_request_target'
+      process.env.GITHUB_EVENT_NAME === 'issue_comment'
     ) {
       await codeReview(lightBot, heavyBot, options, prompts)
     } else if (
